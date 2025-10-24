5bbfa8c0d8ef95137b51487f14c5796cc0a059c0
Dimitar Pashev <dim.pashev@gmail.com>
2025-10-24 20:35:23 +0300

    Add digit prediction and model evaluation

---
+++ handwriting_digit_recognition/README.md
@@ -0,0 +1,33 @@
+# Handwriting Digit Recognition
+
+This project is a machine learning application for recognizing handwritten digits. It utilizes a Convolutional Neural Network (CNN) built with TensorFlow to classify digits from the MNIST dataset.
+
+## Features
+
+-   A deep learning model for digit classification.
+-   Training and evaluation scripts.
+-   Visualization of predictions.
+
+## Requirements
+
+-   Python 3.13+
+-   TensorFlow
+-   Matplotlib
+-   NumPy
+
+## Installation
+
+1.  Clone this repository:
+    
+2.  Install the dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+## Usage
+
+To train the model and see a prediction, run:
+
+```bash
+python main.py
+```
--- handwriting_digit_recognition/main.py
+++ handwriting_digit_recognition/main.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
 
@@ -7,6 +8,9 @@ mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+x_train_reshaped = x_train.reshape((-1, 28, 28, 1))
+x_test_reshaped = x_test.reshape((-1, 28, 28, 1))
 
 model = Sequential([
     Input(shape=(28, 28, 1)),
@@ -24,6 +28,20 @@ model.compile(optimizer='adam',
                 loss='sparse_categorical_crossentropy',
                 metrics=['accuracy']) 
 
+model.fit(x_train_reshaped, y_train, epochs=5, validation_data=(x_test_reshaped, y_test))
+
+test_loss, test_acc = model.evaluate(x_test_reshaped, y_test)
+print(f'\nTest accuracy: {test_acc * 100:.2f}%')
+
+index = 0
+test_image = x_test[index].reshape(1, 28, 28, 1)
+
+prediction = np.argmax(model.predict(test_image), axis=-1)
+plt.imshow(x_test[index], cmap='gray')
+plt.title(f'Predicted Digit: {prediction}')
+plt.show()
+
+# model.summary()
 
 # plt.imshow(x_train[0], cmap='gray')
 # plt.title(f'Label: {y_train[0]}')
